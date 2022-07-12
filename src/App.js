a252cabe63454435dce6231b7bb4f0b3c1abe136
ZwingliCaleb <calebzwingli@gmail.com>
2022-07-12 13:00:34 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info/>
+      <AddItem/>
+      <AddItem/>
+      <AddItem/>
     </div>
   );
 }
 
+function Info(){
+  const title = "This is my title.";
+  const showTitle = true;
+
+  return(
+    <div>
+      <h1>Inventory System</h1>
+      <p>Manage your stuff</p>
+    </div>
+  )
+}
+
+function AddItem(){
+  return(
+    <form>
+      <label for="text-form">Type Something:</label>
+      <input type= "text" id="text-form"/>
+    </form>
+  );
+}
 export default App;
